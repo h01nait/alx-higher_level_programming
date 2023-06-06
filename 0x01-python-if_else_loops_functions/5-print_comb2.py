f24bb7d2b4da7d0d0b84f79cba4ac6d97a1e5340
h01nait <h01nait@gmail.com>
2023-06-06 14:47:16 -0700

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
-    print(f"{number:02}",end="")
+    print("{:>02}".format(number),end="")
     if number != 99:
         print(", ", end="")
     else:
