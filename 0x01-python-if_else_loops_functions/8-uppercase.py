3d44f4b811f4c09c04fa1f1d290636133c766ddd
h01nait <h01nait@gmail.com>
2023-06-06 18:02:27 -0700

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        print("{}".format(chr(ord(letter) - 32) if
+              ord('a') <= ord(letter) <= ord('z') else letter), end='')
+    print()
