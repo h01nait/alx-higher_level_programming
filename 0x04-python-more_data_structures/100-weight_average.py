f7e0509b72b2b367a6f1af2728cff7b7ed00a30e
h01nait <h01nait@gmail.com>
2023-06-16 02:00:08 +0000

    100-weight_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+ght_average(my_list=[]):
+    if not my_list:
+        return 0
+    numerator = sum(score * weight for score, weight in my_list)
+    denominator = sum(weight for _, weight in my_list)
+    return numerator / denominator
